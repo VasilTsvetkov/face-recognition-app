a09e8990b9e53d7004a7f1d8b51e22af7752fe02
Vasil Tsvetkov <vasiltsvetkov20@gmail.com>
2024-03-06 17:58:31 +0200

    Fixed css bug where the word detect goes out of bounds on phone devices

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,8 +11,9 @@ const ImageLinkForm = ({onInputChange, onImageSubmit}) => {
 				<div className='form center pa4 br3 shadow-5'>
 					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
 					<button 
-						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-						onClick={onImageSubmit}>Detect</button>
+						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple truncate'
+						onClick={onImageSubmit}>Detect
+					</button>
 				</div>
 			</div>
     	</div>
